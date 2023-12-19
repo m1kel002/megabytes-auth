256689b88dd426fd1b61645982aadd08239aa01e
Michael Florendo <michael.florendo.002@gmail.com>
2023-12-19 10:08:52 +0800

    feat: add cors headers in response

--- services/login/main.py
+++ services/login/main.py
@@ -1,13 +1,12 @@
 import json
 import boto3
+from botocore.exceptions import ClientError
 from constants.config import USER_POOL_ID, CLIENT_ID
 
 
 def handler(event, context):
     print(f"Event Triggered: {json.dumps(event)}")
-    # response = {"statusCode": 200, "body": json.dumps(body)}
     body = json.loads(event['body'])
-    # response = dict(statusCode=200, body=json.dumps(body))
     try:
         username = body['username']
         password = body['password']
@@ -20,7 +19,24 @@ def handler(event, context):
                                                   'PASSWORD': password
                                               })
         print(f"login successful: {response}")
-        return dict(statusCode=200, body=json.dumps(response))
+        return make_response(json.dumps(response), 200)
+
+    except ClientError as ufe:
+        print(f"User does not exist {str(ufe)}")
+        return make_response(body=json.dumps(
+            dict(message='User does not exists')),
+                             status_code=400)
     except Exception as err:
         print(f"login failed: {str(err)}")
-        return dict(statusCode=500, body=str(err))
+        return make_response(body=json.dumps(body), status_code=500)
+
+
+def make_response(body: str, status_code: int):
+    return {
+        'statusCode': status_code,
+        'body': body,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': True
+        }
+    }
--- services/signup/main.py
+++ services/signup/main.py
@@ -39,35 +39,11 @@ def handler(event, context):
         return make_response({'message': str(err)}, 500)
 
 def make_response(body: dict, code: int):
-    return {'statusCode': code, 'body': json.dumps(body)}
-    
-
-# try:
-#             kwargs = {
-#                 "ClientId": self.client_id,
-#                 "Username": user_name,
-#                 "Password": password,
-#                 "UserAttributes": [{"Name": "email", "Value": user_email}],
-#             }
-#             if self.client_secret is not None:
-#                 kwargs["SecretHash"] = self._secret_hash(user_name)
-#             response = self.cognito_idp_client.sign_up(**kwargs)
-#             confirmed = response["UserConfirmed"]
-#         except ClientError as err:
-#             if err.response["Error"]["Code"] == "UsernameExistsException":
-#                 response = self.cognito_idp_client.admin_get_user(
-#                     UserPoolId=self.user_pool_id, Username=user_name
-#                 )
-#                 logger.warning(
-#                     "User %s exists and is %s.", user_name, response["UserStatus"]
-#                 )
-#                 confirmed = response["UserStatus"] == "CONFIRMED"
-#             else:
-#                 logger.error(
-#                     "Couldn't sign up %s. Here's why: %s: %s",
-#                     user_name,
-#                     err.response["Error"]["Code"],
-#                     err.response["Error"]["Message"],
-#                 )
-#                 raise
-#         return confirmed
+    return {
+        'statusCode': code,
+        'body': json.dumps(body),
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': True
+        }
+    }
